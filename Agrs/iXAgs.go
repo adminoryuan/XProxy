941dfbbf892fce2680d018a38176d5336a559895
adminoryuan <1317332365@qq.com>
2022-04-03 20:38:09 +0800

    insert iXages

--- Agrs/iXAgs.go
+++ Agrs/iXAgs.go
@@ -1,16 +1,39 @@
 package agrs
 
+import (
+	"errors"
+	"runtime"
+	"strconv"
+)
+
 type iXargs interface {
-	Commit()
+	Cmmd()
 }
 type AgrsAgent struct {
 	Ip string
-
-	ProceMax int
 }
 type xargsImp struct {
+	agent   AgrsAgent
+	cmdFunc map[string]func(val string)
 }
 
+func NewXargs() iXargs {
+	imp := xargsImp{}
+
+	imp.cmdFunc = make(map[string]func(string))
+
+	imp.cmdFunc["-x"] = func(val string) {
+		runNumber, er := strconv.Atoi(val)
+		if er != nil {
+			panic(errors.New("输入错误!!!"))
+		}
+		runtime.GOMAXPROCS(runNumber)
+	}
+	imp.cmdFunc["-a"] = func(val string) {
+
+	}
+	return imp
+}
 func (a xargsImp) Cmmd() {
 
 }
